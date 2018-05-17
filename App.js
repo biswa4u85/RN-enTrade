a35a5f098ee9d2f139690019388339002a5ec7e1
Biswajit Sahoo <biswa@techlift.in>
2018-05-17 11:33:23 +0530

    update new camera

--- App.js
+++ App.js
@@ -1,65 +1,108 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+export default class App extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+    openCamera: false,
+    photo: {},
+    comments: 'Add Comments Here...',
+  };
 
-import Camera from 'react-native-camera'
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Camera
-          ref={(cam) => {
-            this.camera = cam
-          }}
-          style={styles.view}
-          aspect={Camera.constants.Aspect.fill}>
-            <Text 
-            style={styles.capture} 
-            onPress={this.takePicture.bind(this)}>
-              [CAPTURE_IMAGE]
-            </Text>
-        </Camera>
-      </View>
-    );
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
   }
 
-  takePicture() {
-    const options = {}
+  render() {
+    const { hasCameraPermission, openCamera } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else if (hasCameraPermission === true && openCamera === true) {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera ref={ref => { this.camera = ref; }} style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginLeft: 10, marginTop: 50, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+          <View>
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({
+                  openCamera: false,
+                  photo: this.camera.takePictureAsync()
+                });
+              }}>
+              <Text style={{ fontSize: 18, margin: 10 }}>Save Photo</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({
+                  openCamera: false,
+                  photo: {}
+                });
+              }}>
+              <Text style={{ fontSize: 18, margin: 10 }}>Cancel Photo</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )
+    } else {
+      return (
 
-    this.camera.capture({metadata: options}).then((data) => {
-      console.log(data)
-    }).catch((error) => {
-      console.log(error)
-    })
+        <View style={{ flex: 1, marginTop: 100 }}>
+          <View>
+            {/* <Text>{this.state.photo}</Text> */}
+            <TouchableOpacity
+              style={{ margin: 20 }}
+              onPress={() => {
+                this.setState({
+                  openCamera: true
+                });
+              }}>
+              <Text>Take Photo</Text>
+            </TouchableOpacity>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+              onChangeText={(comments) => this.setState({ comments })}
+              value={this.state.comments}
+            />
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({
+
+                });
+              }}>
+              <Text style={{ fontSize: 18, margin: 10 }}>Save Data</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row'
-  },
-  view: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: 'steelblue',
-    borderRadius: 10,
-    color: 'red',
-    padding: 15,
-    margin: 45
-  }
-});
