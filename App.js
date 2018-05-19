93e9f713ea420baa16e0d8b8992edf6e0bcbd416
Biswajit Sahoo <biswa@techlift.in>
2018-05-19 16:04:10 +0530

    update files

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { Text, TextInput, Image, View, TouchableOpacity } from 'react-native';
 import { Camera, Permissions } from 'expo';
+import { Dropdown } from 'react-native-material-dropdown';
 
 export default class App extends React.Component {
+
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
     openCamera: false,
-    photo: 'Image',
-    comments: 'Add Comments Here...',
+    photo: null,
+    comments: '',
   };
 
   async componentWillMount() {
@@ -16,8 +18,32 @@ export default class App extends React.Component {
     this.setState({ hasCameraPermission: status === 'granted' });
   }
 
+  showImg() {
+    console.log(this.state.photo)
+    if (this.state.photo) {
+      return (
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+        />
+      )
+    } else {
+      return (
+        <Text style={{ fontSize: 15, padding: 10 }}>No Image</Text>
+      )
+    }
+  }
+
   render() {
     const { hasCameraPermission, openCamera } = this.state;
+    let data = [{
+      value: 'Type 01',
+    }, {
+      value: 'Type 02',
+    }, {
+      value: 'Type 03',
+    }];
+
     if (hasCameraPermission === null) {
       return <View />;
     } else if (hasCameraPermission === false) {
@@ -46,7 +72,7 @@ export default class App extends React.Component {
                     photo: this.camera.takePictureAsync()
                   });
                 }}>
-                <Text style={{ fontSize: 18, margin: 10, color: 'white' }}>Save</Text>
+                <Text style={{ fontSize: 18, margin: 10, color: 'white' }}>Keep</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 style={{
@@ -55,10 +81,10 @@ export default class App extends React.Component {
                 onPress={() => {
                   this.setState({
                     openCamera: false,
-                    photo: {}
+                    photo: null
                   });
                 }}>
-                <Text style={{ fontSize: 18, margin: 10, color: 'white' }}>Cancel</Text>
+                <Text style={{ fontSize: 18, margin: 10, color: 'white' }}>Discard</Text>
               </TouchableOpacity>
             </View>
           </Camera>
@@ -68,7 +94,7 @@ export default class App extends React.Component {
       return (
         <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', marginTop: 100 }}>
           <View>
-            <Text>{this.state.photo}</Text>
+            {this.showImg()}
             <TouchableOpacity
               style={{ backgroundColor: 'gray', padding: 10, marginTop: 5, borderRadius: 5 }}
               onPress={() => {
@@ -79,13 +105,21 @@ export default class App extends React.Component {
               <Text style={{ fontSize: 15, color: 'white' }}>Take Photo</Text>
             </TouchableOpacity>
           </View>
-          <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-            onChangeText={(comments) => this.setState({ comments })}
-            value={this.state.comments}
-          />
+          <View
+            style={{ width: 200 }}
+          >
+            <Dropdown
+              label='Select Type'
+              data={data}
+            />
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', padding: 10, borderWidth: 1 }}
+              onChangeText={(comments) => this.setState({ comments })}
+              value={this.state.comments}
+            />
+          </View>
           <TouchableOpacity
-            style={{ backgroundColor: 'gray', padding: 10, margin: 10, borderRadius: 5 }}
+            style={{ backgroundColor: 'gray', padding: 10, margin: 10, borderRadius: 5, marginBottom: 100 }}
             onPress={() => {
               this.setState({
 
